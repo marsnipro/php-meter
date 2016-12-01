ec85bff04c4f0ce88b46b5211f73a9e8c23445fa
Sandro Marcell <smarcell@mail.com>
2016-12-01 16:05:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<title>php-bwm</title>
+	<title>php-meter</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="author" content="Sandro Marcell" />
